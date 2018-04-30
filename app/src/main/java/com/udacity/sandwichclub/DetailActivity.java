113cf10edbd580b8b0da7c3fda6ebbda49bdfa87
Belle Lee <letiya66@gmail.com>
2018-04-29 21:52:58 -0700

    Show the details of each sandwich once it is selected by JSON parsing. Design an detail activity layout.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView alsoKnownAs;
+    private TextView placeOfOrigin;
+    private TextView ingredients;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        alsoKnownAs = findViewById(R.id.also_known_tv);
+        placeOfOrigin = findViewById(R.id.origin_tv);
+        ingredients = findViewById(R.id.ingredients_tv);
+        description = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -41,6 +56,33 @@ public class DetailActivity extends AppCompatActivity {
             // Sandwich data unavailable
             closeOnError();
             return;
+        } else {
+            ArrayList<String> alsoKnownAsList = (ArrayList<String>) sandwich.getAlsoKnownAs();
+            for (int i = 0; i < alsoKnownAsList.size(); i++) {
+                String name = alsoKnownAsList.get(i);
+                name = name.replace("[","");
+                name = name.replace("]", "");
+                name = name.replace("\"", "");
+                if (i == alsoKnownAsList.size() - 1) {
+                    alsoKnownAs.append(name);
+                } else {
+                    alsoKnownAs.append(name + ", ");
+                }
+            }
+            placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+            ArrayList<String> ingredientsList = (ArrayList<String>) sandwich.getIngredients();
+            for (int j = 0; j < ingredientsList.size(); j++) {
+                String ingredient = ingredientsList.get(j);
+                ingredient = ingredient.replace("[", "");
+                ingredient = ingredient.replace("]", "");
+                ingredient = ingredient.replace("\"", "");
+                if (j == ingredientsList.size() - 1) {
+                    ingredients.append(ingredient);
+                } else {
+                    ingredients.append(ingredient + ", ");
+                }
+            }
+            description.setText(sandwich.getDescription());
         }
 
         populateUI();
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,51 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            String mainName = jsonObject.getJSONObject("name").getString("mainName");
+
+            String alsoKnownAs = jsonObject.getJSONObject("name").getString("alsoKnownAs");
+            List<String> alsoKnownAsList = null;
+            String[] alsoKnownAsArray = alsoKnownAs.split(",");
+            if (alsoKnownAsArray.length != 0) {
+                alsoKnownAsList = new ArrayList<String>();
+                for (String s:alsoKnownAsArray) {
+                    alsoKnownAsList.add(s);
+                }
+            }
+
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+
+            String ingredients = jsonObject.getString("ingredients");
+            List<String> ingredientsList = null;
+            String[] ingredientsArray = ingredients.split(",");
+            if (ingredientsArray.length != 0) {
+                ingredientsList = new ArrayList<String>();
+                for(String s:ingredientsArray) {
+                    ingredientsList.add(s);
+                }
+            }
+            sandwich = new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
